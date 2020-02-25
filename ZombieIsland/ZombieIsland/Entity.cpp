24beeeb760a1fed892159159ed6979915fabec85
Jones I (FCES) <30005209@glam.ac.uk>
2020-02-25 11:57:49 +0000

    Version 1

--- ZombieIsland/ZombieIsland/Entity.cpp
+++ ZombieIsland/ZombieIsland/Entity.cpp
@@ -8,3 +8,33 @@ Entity::Entity()
 Entity::~Entity()
 {
 }
+
+void Entity::setBrd(const vector<Entity>* pBrd)
+{
+	this->brd = pBrd;
+}
+
+void Entity::setPos(const vector<Entity>::iterator newPos)
+{
+	this->pos = newPos;
+}
+
+void Entity::setSym(const char newSym)
+{
+	this->sym = newSym;
+}
+
+const char Entity::getSymbol(void)
+{
+	return sym;
+}
+
+const vector<Entity>* Entity::getBoard(void)
+{
+	return brd;
+}
+
+const vector<Entity>::iterator Entity::getPos(void)
+{
+	return pos;
+}
