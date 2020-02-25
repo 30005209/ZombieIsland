29299dd1608e20c3c5f89048153943f08308ef26
Jones I (FCES) <30005209@glam.ac.uk>
2020-02-25 11:19:56 +0000

    begun working on Entity

---
+++ ZombieIsland/ZombieIsland/Entity.h
@@ -0,0 +1,16 @@
+#ifndef Entity_H
+#define Entity_H
+
+//A virtual class that contains aspects common across all future objects on the board
+class Entity
+{
+public:
+	Entity();
+	~Entity();
+};
+
+
+
+
+
+#endif
